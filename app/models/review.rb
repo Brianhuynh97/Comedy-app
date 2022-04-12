a8716e3c3c4c7fc4efc1e1f2b86ea5d10576ec0f
Connor Sanderson <connor.sanderson@outlook.com>
2022-04-12 21:43:59 +1000

    removed comment validation on review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :show
   belongs_to :user
 
-  validates :rating, :comment, presence: true
+  validates :rating, presence: true
   validates :rating, numericality: { only_integer: true }, inclusion: [0,1,2,3,4,5]
 end
