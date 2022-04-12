30844f81745bb980b36043ecb33e70f77670b17a
Connor Sanderson <connor.sanderson@outlook.com>
2022-04-12 21:17:39 +1000

    updated reviews model and controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,46 +1,61 @@
 class ReviewsController < ApplicationController
-
+  before_action :find_review, only: [:edit, :update]
+  before_action :find_show, only: [:new, :create, :update]
   def new
     @review = Review.new
-    @show = Show.find(params[:show_id])
+    # @show = Show.find(params[:show_id])
   end
 
   def create
     @review = Review.new(review_params)
-    @show = Show.find(params[:show_id])
+    # @show = Show.find(params[:show_id])
     @review.show = @show
+
+    #confirm this line
+    @review.user = current_user
+
     @review.save
     if @review.save
       redirect_to show_path(@show)
     else
-      render :new
+      # render :show as review form will be on the show page
+      render :show
     end
   end
 
   def edit
-    @review = Review.find(params[:id])
+    # @review = Review.find(params[:id])
   end
 
   def update
-    @review = Review.find(params[:id])
-    @show = Show.find(params[:show_id])
+    # @review = Review.find(params[:id])
+    # @show = Show.find(params[:show_id])
     @review.update(review_params)
     if @review.update(review_params)
       redirect_to show_path(@show)
     else
-      render :edit
+      # render :show as review form will be on the show page
+      render :show
     end
   end
 
   def destroy
     @review.destroy
+    # may want to redirect to the show_path(@show)
     redirect_to shows_path
   end
 
   private
 
+  def find_review
+    @review = Review.find(params[:id])
+  end
+
+  def find_show
+    @show = Show.find(params[:show_id])
+  end
+
   def review_params
     params.require(:review).permit(:rating, :comment)
   end
-
 end
