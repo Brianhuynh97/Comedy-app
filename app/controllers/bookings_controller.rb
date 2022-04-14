f1e0729bb4c492d8e32956acdea46aee861e4333
Brian Huynh <vuduchv@gmail.com>
2022-04-14 18:23:16 +1000

    Booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,29 @@
 class BookingsController < ApplicationController
-  
+  def create
+    @show = Show.find(params[:show_id])
+    @booking = Booking.new(booking_params)
+    @booking.show = @show
+    @booking.user = current_user
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      redirect_to show_path(@show)
+    end
+  end
+
+  def destroy
+    set_booking
+    @booking.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:seat_quantity)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :show
   belongs_to :user
+  has_one :review
+  validates :seat_quantity, presence: true
 end
