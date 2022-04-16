acf7decf437473e47b083c1596ade4c04b45cf70
Sarah Pelham <sarah.pelham@gmail.com>
2022-04-16 10:25:33 +1000

    installed geocoder gem, created env file and added it to gitignore, updated dotenv groups

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,4 +5,6 @@ class Show < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true, length: { in: 6..600 }
   validates :price, :location, :datetime, :capacity, presence: true
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
