42cc024d993c15fc86b2029174df84c19932599e
Brian Huynh <vuduchv@gmail.com>
2022-04-21 00:29:06 +1000

    update feature and form

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/lists", to: "pages#lists"
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
+  get "/feedback", to: "pages#feedback"
 
   resources :shows do
     resources :reviews, only: %i[new create]
