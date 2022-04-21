195e208fd9fbf697ddffc6c2c949320997fa02a2
Sarah Pelham <sarah.pelham@gmail.com>
2022-04-21 22:02:00 +1000

    updated profile to lists path, added Ash's booking updates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,10 @@ class Booking < ApplicationRecord
   belongs_to :user
   has_one :review
   validates :seat_quantity, presence: true
+
+  def ensure_seat_qty_not_exceed_capacity
+    current_capacity = show.bookings.to_a.sum(0){|booking| booking.seat_quantity}
+    remaining_capacity = show.capacity - current_capacity
+    errors.add(:seat_quantity, "Cannot exceed maximum capacity") if seat_quantity > remaining_capacity
+  end
 end
